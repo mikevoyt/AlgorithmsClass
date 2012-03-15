915c74f1b2d6bb06febc99bc80a3fdb0cce0603a
Mike Voytovich <mikev@duffresearch.com>
2012-03-14 21:01:03 -0700

    skeleton

---
+++ src/Algorithm.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+/**
+ */
+public class Algorithm {
+	public static int countInversions(List<Integer> values) {
+		int count = 0;
+		if (values == null) return count;
+		System.out.println("values len:" + values.size());
+		return count;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+		List<String> lines = null;
+		try {
+			File file = new File("IntegerArray.txt");
+			lines = Files.readLines(file, Charsets.UTF_8);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+		List<Integer> values = new ArrayList<Integer>();
+		for (String line : lines) {
+			values.add(Integer.parseInt(line));
+		}
+		
+		Algorithm.countInversions(values);
+	}
+}
