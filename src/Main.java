95c05337cf116def6326828721784bb4ccc66023
Mike Voytovich <mikev@duffresearch.com>
2012-03-15 08:55:04 -0700

    still debugging

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		List<String> lines = null;
 		try {
-			File file = new File("IntegerArray.txt");
+			File file = new File("IntegerArrayShort.txt");
 			lines = Files.readLines(file, Charsets.UTF_8);
 		} catch (IOException e) {
 			System.out.println(e);
